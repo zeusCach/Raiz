96158e3e86035d578755685fc05f312e6e972e8d
Zeus Cach <zeus.cach@outlook.com>
2026-08-23 00:11:19 -0500

    feat: add RegistroPage component for user registration

--- raiz-frontend/src/app/router.tsx
+++ raiz-frontend/src/app/router.tsx
@@ -3,6 +3,7 @@ import { Feed } from '../features/feed/components/Feed';
 import { PostDetailPage } from '../features/postCultura/page/PostDetailPage';
 import { AppLayout } from './AppLayout';
 import { PostForm } from '../features/postCultura/components/postCard/PostFrom';
+import { RegistroPage } from '../features/auth/page/RegisterPage';
 
 
 export const router = createBrowserRouter([
@@ -14,6 +15,8 @@ export const router = createBrowserRouter([
       { path: 'feed', element: <Feed /> },
       { path: 'post/:id', element: <PostDetailPage /> },
       { path: 'publicar', element: <PostForm /> },
+    
     ],
   },
+  { path: 'registro', element: <RegistroPage /> },
 ]);
